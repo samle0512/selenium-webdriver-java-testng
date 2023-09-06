ebdf1c936fa5f9f85746fe578412d0d102ad560f
samle0512 <lethimaisam35k14@gmail.com>
2023-09-06 15:40:33 +0700

    Add TC02

--- selenium/webdriver/Topic_02_XPath_Css.java
+++ selenium/webdriver/Topic_02_XPath_Css.java
@@ -1,4 +1,12 @@
 package webdriver;
 
+import org.testng.annotations.Test;
+
 public class Topic_02_XPath_Css {
+    @Test
+    public void TC02()
+    {
+        System.out.println("Hello");
+    }
+
 }
